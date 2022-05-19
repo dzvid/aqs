f04f0c073be86a7d11ea39b61eb27f6af64b7a28
David <oliveiradavid007@gmail.com>
2022-05-19 02:40:58 -0400

    wip: Fix gps

--- endDevice/src/Gps.cpp
+++ endDevice/src/Gps.cpp
@@ -17,33 +17,75 @@ void Gps::init() {
   _serial_gps.begin(GPS_BAUDRATE_SERIAL, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
 }
 
+static void smartDelay(unsigned long ms) {
+  unsigned long start = millis();
+  do {
+    while (_serial_gps.available())
+      _gps.encode(_serial_gps.read());
+  } while (millis() - start < ms);
+}
+
 TGPS_DATA Gps::getReading() {
   TGPS_DATA currentGpsData;
 
-  while (_serial_gps.available() > 0) {
-    if (_gps.encode(_serial_gps.read())) {
-      if (_gps.location.isValid() && _gps.date.isValid() && _gps.time.isValid()) {
-        currentGpsData.latitude = _gps.location.lat();
-        currentGpsData.longitude = _gps.location.lng();
-        currentGpsData.year = _gps.date.year();
-        currentGpsData.month = _gps.date.month();
-        currentGpsData.day = _gps.date.day();
-        currentGpsData.hours = _gps.time.hour();
-        currentGpsData.minutes = _gps.time.minute();
-        currentGpsData.seconds = _gps.time.second();
-        currentGpsData.readSuccess = true;
-      } else {
-        currentGpsData.readSuccess = false;
-      }
+  currentGpsData.latitude = _gps.location.lat();
+  currentGpsData.longitude = _gps.location.lng();
+  currentGpsData.year = _gps.date.year();
+  currentGpsData.month = _gps.date.month();
+  currentGpsData.day = _gps.date.day();
+  currentGpsData.hours = _gps.time.hour();
+  currentGpsData.minutes = _gps.time.minute();
+  currentGpsData.seconds = _gps.time.second();
+  currentGpsData.readSuccess = true;
 
-      return currentGpsData;
-    }
-  }
+  Serial.print("Latitude  : ");
+  Serial.println(_gps.location.lat(), 5);
+  Serial.print("Longitude : ");
+  Serial.println(_gps.location.lng(), 4);
+  Serial.print("Satellites: ");
+  Serial.println(_gps.satellites.value());
+  Serial.print("Altitude  : ");
+  Serial.print(_gps.altitude.feet() / 3.2808);
+  Serial.println("M");
+  Serial.print("Time      : ");
+  Serial.print(_gps.time.hour());
+  Serial.print(":");
+  Serial.print(_gps.time.minute());
+  Serial.print(":");
+  Serial.println(_gps.time.second());
+  Serial.print("Speed     : ");
+  Serial.println(_gps.speed.kmph());
+  Serial.println("**********************");
 
-  if (millis() > 5000 && _gps.charsProcessed() < 10) {
-    Serial.println(F("No GPS detected: check wiring."));
-    currentGpsData.readSuccess = false;
-  }
+  smartDelay(1000);
+
+  if (millis() > 5000 && _gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
+
+  // while (_serial_gps.available() > 0) {
+  //   if (_gps.encode(_serial_gps.read())) {
+  //     if (_gps.location.isValid() && _gps.date.isValid() && _gps.time.isValid()) {
+  //       currentGpsData.latitude = _gps.location.lat();
+  //       currentGpsData.longitude = _gps.location.lng();
+  //       currentGpsData.year = _gps.date.year();
+  //       currentGpsData.month = _gps.date.month();
+  //       currentGpsData.day = _gps.date.day();
+  //       currentGpsData.hours = _gps.time.hour();
+  //       currentGpsData.minutes = _gps.time.minute();
+  //       currentGpsData.seconds = _gps.time.second();
+  //       currentGpsData.readSuccess = true;
+  //     } else {
+  //       currentGpsData.readSuccess = false;
+  //     }
+
+  //     return currentGpsData;
+  //   }
+  // }
+
+  // if (millis() > 5000 && _gps.charsProcessed() < 10) {
+  //   Serial.println(F("No GPS detected: check wiring."));
+  //   currentGpsData.readSuccess = false;
+  // }
 
   return currentGpsData;
 }
