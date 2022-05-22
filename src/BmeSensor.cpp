a0e7bb9ff59b0d60531b8a8abf51c13c3cb3d72e
David <oliveiradavid007@gmail.com>
2022-05-22 15:58:50 -0400

    chore: Setup

---
+++ src/BmeSensor.cpp
@@ -0,0 +1,61 @@
+#include "BmeSensor.h"
+
+BmeSensor::BmeSensor() {}
+
+void BmeSensor::init() {
+  // BME
+  if (!_bme.begin(BME280_I2C_ADDR)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1)
+      ;
+  }
+}
+
+TBME_DATA BmeSensor::getReading() {
+  TBME_DATA reading;
+
+  // Serial.print("Temperature = ");
+  // Serial.print(_bme.readTemperature());
+  // Serial.println("*C");
+
+  // Serial.print("Pressure = ");
+  // Serial.print(_bme.readPressure() / 100.0F);
+  // Serial.println("hPa");
+
+  // Serial.print("Approx. Altitude = ");
+  // Serial.print(_bme.readAltitude(SEALEVELPRESSURE_HPA));
+  // Serial.println("m");
+
+  // Serial.print("Humidity = ");
+  // Serial.print(_bme.readHumidity());
+  // Serial.println("%");
+
+  // Serial.println();
+
+  reading.readSuccess = true;
+  reading.temperature = _bme.readTemperature();
+  reading.pressure = _bme.readPressure() / 100.0F;
+  reading.humidity = _bme.readHumidity();
+
+  return reading;
+}
+
+void BmeSensor::displayData(TBME_DATA bmeData) {
+  if (bmeData.readSuccess) {
+    Serial.print("Temperature = ");
+    Serial.print(bmeData.temperature);
+    Serial.println("*C");
+
+    Serial.print("Pressure = ");
+    Serial.print(bmeData.pressure);
+    Serial.println("hPa");
+
+    Serial.print("Humidity = ");
+    Serial.print(bmeData.humidity);
+    Serial.println("%");
+
+    Serial.println();
+  } else {
+    Serial.println("BME:: No data.");
+  }
+}
---
+++ src/Board.h
@@ -0,0 +1,28 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#define BUTTON_PIN_MASK GPIO_SEL_38
+
+#define I2C_SDA 21
+#define I2C_SCL 22
+
+#define PMU_IRQ 35
+
+#define RADIO_SCLK_PIN 5
+#define RADIO_MISO_PIN 19
+#define RADIO_MOSI_PIN 27
+#define RADIO_CS_PIN 18
+#define RADIO_DI0_PIN 26
+#define RADIO_RST_PIN 23
+#define RADIO_DIO1_PIN 33
+#define RADIO_BUSY_PIN 32
+
+class Board {
+ public:
+  Board();
+
+  bool initPMU();
+  void init();
+};
+
+#endif /* _BOARD_H_ */
