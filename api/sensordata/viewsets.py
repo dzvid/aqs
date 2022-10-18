007d91ccbe6aa72e1a5249d70dbc184d693fd59c
David <oliveiradavid.dev@gmail.com>
2022-10-18 00:15:28 -0400

    api: add sensors list

--- api/sensordata/viewsets.py
+++ api/sensordata/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
 from sensordata.filters import SensorDataFilter
 from sensordata.models import SensorData
@@ -10,3 +11,7 @@ class SensorDataViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = SensorDataSerializer
     filterset_class = SensorDataFilter
     permission_classes = []
+
+    @action(methods=['get'], detail=False)
+    def list_sensors(self, request, *args,  **kwargs):
+        return self.get_queryset().values('device_name').distinct()
