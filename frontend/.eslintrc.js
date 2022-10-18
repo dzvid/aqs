b8befce115e1f418eb87b4bdbabc8bde1f0e7776
David <oliveiradavid.dev@gmail.com>
2022-10-17 22:46:52 -0400

    frontend: change to js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,19 +1,24 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
   root: true,
   env: {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    '@vue/eslint-config-prettier',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    prettier: {
+      single: true,
+    },
   },
 };
