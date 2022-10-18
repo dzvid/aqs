28d1ebbebf0fdd290d2e7633acf3bbd6843c147f
David <oliveiradavid.dev@gmail.com>
2022-10-18 02:13:32 -0400

    frontend: wip add cards

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,45 +1,191 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar app color="grey lighten-4" flat>
+      <h1>
+        <span class="text-h2 text-bold">AQS</span>
+        <span class="text-h2 primary--text">UEA</span>
+      </h1>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld />
+    <v-main class="grey lighten-4">
+      <v-container class="">
+        <v-row dense>
+          <v-col cols="12">
+            <v-card flat>
+              <v-card-title class="text-h5"> Selecione um sensor </v-card-title>
+              <v-card-text>
+                <v-form>
+                  <v-row>
+                    <v-col cols="12" sm="4" md="3">
+                      <v-select
+                        :items="[]"
+                        label="Sensores"
+                        outlined
+                        hide-details
+                      ></v-select>
+                    </v-col>
+                    <v-col cols="12" sm="4" md="3">
+                      <v-menu
+                        v-model="menu2"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="date"
+                            label="Dia"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                            outlined
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="date"
+                          @input="menu2 = false"
+                        ></v-date-picker>
+                      </v-menu>
+                    </v-col>
+                    <v-col cols="12" sm="auto">
+                      <v-btn class="primary" depressed x-large>
+                        {{ 'Buscar' }}
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row dense class="mt-4">
+          <v-col cols="12">
+            <v-card class="mx-auto" flat>
+              <v-card-title class="text-h5">
+                Índice da Qualidade do Ar - IQA
+              </v-card-title>
+
+              <v-card-text>
+                <v-row align="center">
+                  <v-col class="text-h2" cols="6"> 23&deg;C </v-col>
+                  <v-col cols="6">
+                    <v-img
+                      src="https://cdn.vuetifyjs.com/images/cards/sun.png"
+                      alt="Sunny image"
+                      width="92"
+                    ></v-img>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-send</v-icon>
+                </v-list-item-icon>
+                <v-list-item-subtitle>23 km/h</v-list-item-subtitle>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-cloud-download</v-icon>
+                </v-list-item-icon>
+                <v-list-item-subtitle>48%</v-list-item-subtitle>
+              </v-list-item>
+
+              <v-slider
+                v-model="time"
+                :max="6"
+                :tick-labels="labels"
+                class="mx-4"
+                ticks
+              ></v-slider>
+
+              <v-list class="transparent">
+                <v-list-item v-for="item in forecast" :key="item.day">
+                  <v-list-item-title>{{ item.day }}</v-list-item-title>
+
+                  <v-list-item-icon>
+                    <v-icon>{{ item.icon }}</v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-subtitle class="text-right">
+                    {{ item.temp }}
+                  </v-list-item-subtitle>
+                </v-list-item>
+              </v-list>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-btn text> Full Report </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row dense>
+          <v-col cols="12">
+            <v-card class="mx-auto" flat>
+              <v-card-text>
+                <v-list-item two-line>
+                  <v-list-item-content>
+                    <v-list-item-title class="text-h5">
+                      PM 2.5
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row dense>
+          <v-col cols="12">
+            <v-card class="mx-auto" flat>
+              <v-list-item two-line>
+                <v-list-item-content>
+                  <v-list-item-title class="text-h5">
+                    Temperatura
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row dense>
+          <v-col cols="12">
+            <v-card class="mx-auto" flat>
+              <v-list-item two-line>
+                <v-list-item-content>
+                  <v-list-item-title class="text-h5">
+                    Umidade
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
+  components: {},
   data: () => ({
-    //
+    date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+      .toISOString()
+      .substr(0, 10),
+    menu: false,
+    modal: false,
+    menu2: false,
   }),
 };
 </script>
