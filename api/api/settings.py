25fedf790d0d51807e546341a8017f2862a58e3b
David <oliveiradavid.dev@gmail.com>
2022-10-19 01:29:39 -0400

    api: fix cors

--- api/api/settings.py
+++ api/api/settings.py
@@ -26,7 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('DJANGO_ALLOWED_HOSTS', '*')]
 
-CORS_ORIGIN_ALLOW_ALL = [os.environ.get('DJANGO_CORS_ORIGIN_ALLOW_ALL', True)]
+CORS_ORIGIN_ALLOW_ALL = os.environ.get('DJANGO_CORS_ORIGIN_ALLOW_ALL', True)
 
 # Application definition
 
