1e9a8be70357666fc8a366153672d15592cdd5f0
David <oliveiradavid.dev@gmail.com>
2022-10-20 00:54:23 -0400

    api: serialize as json

--- api/sensordata/serializers.py
+++ api/sensordata/serializers.py
@@ -4,9 +4,12 @@ from sensordata.models import SensorData
 
 
 class SensorDataSerializer(serializers.ModelSerializer):
+    object = serializers.JSONField(read_only=True)
+
     class Meta:
         model = SensorData
         fields = '__all__'
+
 
 class SensorsSerializer(serializers.ModelSerializer):
     class Meta:
