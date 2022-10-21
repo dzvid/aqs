62989792294d4f83d6a51528ba394c3a808d6100
David <oliveiradavid.dev@gmail.com>
2022-10-21 15:15:30 -0400

    api: fix jsonfield import

--- api/sensordata/models.py
+++ api/sensordata/models.py
@@ -1,11 +1,10 @@
-from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 
 class SensorData(models.Model):
     id = models.UUIDField(primary_key=True)
     device_name = models.CharField(max_length=255)
-    object = JSONField()
+    object = models.JSONField(null=True)
 
     def __str__(self):
         return self.device_name
