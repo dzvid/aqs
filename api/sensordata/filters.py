c1ea92555709e17393e0246f933abe2eda8ae2c2
David <oliveiradavid.dev@gmail.com>
2022-10-21 15:13:04 -0400

    api: fix

--- api/sensordata/filters.py
+++ api/sensordata/filters.py
@@ -3,7 +3,7 @@ from django_filters import FilterSet, CharFilter
 from sensordata.models import SensorData
 
 
-class SensorDataFilter(FilterSet):
-    class Meta:
-        model = SensorData
-        fields = '__all__'
+# class SensorDataFilter(FilterSet):
+#     class Meta:
+#         model = SensorData
+#         fields = '__all__'
