0a1dd0428fecd2e37856f5da2cd3899f11e519b2
David <oliveiradavid.dev@gmail.com>
2022-10-21 15:13:42 -0400

    api: fix

--- api/sensordata/viewsets.py
+++ api/sensordata/viewsets.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.status import HTTP_200_OK
 
-from sensordata.filters import SensorDataFilter
+# from sensordata.filters import SensorDataFilter
 from sensordata.models import SensorData
 from sensordata.serializers import SensorDataSerializer, SensorsSerializer
 
