8b3e7286765dbadffb1c5cc673eff1fa9e861e30
David <oliveiradavid.dev@gmail.com>
2022-10-21 23:26:22 -0400

    api: fix

--- api/sensordata/viewsets.py
+++ api/sensordata/viewsets.py
@@ -41,7 +41,8 @@ class SensorDataViewSet(viewsets.ReadOnlyModelViewSet):
 
         readings_in_date = []
         for reading in readings:
-            if reading.object is not None:
+            if reading.object is not None and reading.object.data is not None:
+                print(reading.object)
                 reading_date = datetime.strptime(reading.object.data.dt_collected_at, '%Y-%m-%dT%H:%M:%SZ')
                 if start_date < reading_date < end_date:
                     readings_in_date.append(reading)
