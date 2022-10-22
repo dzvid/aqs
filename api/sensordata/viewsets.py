7672b8c831932fe14b0615b72b17d537d4d7db1e
David <oliveiradavid.dev@gmail.com>
2022-10-21 22:50:14 -0400

    api: add

--- api/sensordata/viewsets.py
+++ api/sensordata/viewsets.py
@@ -31,10 +31,12 @@ class SensorDataViewSet(viewsets.ReadOnlyModelViewSet):
         start_date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
         end_date = start_date + timedelta(days=1)
 
+        print(self.get_queryset().filter().first())
+
         readings = self.get_queryset() \
             .filter(device_name__iexact=id_sensor,
-                    object__data__dt_collected_at__gte=str(start_date)+'Z',
-                    object__data__dt_collected_at__lte=str(end_date)+'Z') \
+                    object__data__dt_collected_at__gte=start_date,
+                    object__data__dt_collected_at__lte=end_date) \
             .order_by('object__data__dt_collected_at')
         serializer = SensorDataSerializer(readings, many=True)
         return JsonResponse(serializer.data, status=HTTP_200_OK, safe=False)
